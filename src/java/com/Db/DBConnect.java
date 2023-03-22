490892f86dd8902213ce871595e796986295d299
Aman <amnegi@gmail.com>
2023-03-22 13:42:32 +0530

    Database Connection

--- src/java/com/Db/DBConnect.java
+++ src/java/com/Db/DBConnect.java
@@ -4,10 +4,28 @@
  */
 package com.Db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author 91771
  */
 public class DBConnect {
-    
+        private static Connection conn;
+    public static Connection getCOnn()
+    {
+        try{
+        if(conn==null)
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube\",\"root\",\"\"");
+        }
+     }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
